9dff67007561e3ec17d3a9c2d4d062f5392fde69
Joseph Paul Cohen <joseph@josephpcohen.com>
2020-04-29 22:58:54 -0400

    update script to use cuda and output features

--- scripts/process_image.py
+++ scripts/process_image.py
@@ -11,6 +11,7 @@ import skimage
 import pprint
 
 import torch
+import torch.nn.functional as F
 import torchvision, torchvision.transforms
 
 import torchxrayvision as xrv
@@ -19,7 +20,8 @@ parser = argparse.ArgumentParser()
 parser.add_argument('-f', type=str, default="", help='')
 parser.add_argument('img_path', type=str)
 parser.add_argument('-weights', type=str,default="all")
-parser.add_argument('-cuda', type=bool, default=False, help='')
+parser.add_argument('-feats', default=False, help='', action='store_true')
+parser.add_argument('-cuda', default=False, help='', action='store_true')
 
 cfg = parser.parse_args()
 
@@ -44,9 +46,26 @@ img = transform(img)
 
 
 model = xrv.models.DenseNet(weights=cfg.weights)
-with torch.no_grad():
-    out = model(torch.from_numpy(img).unsqueeze(0)).cpu()
 
-pprint.pprint(dict(zip(xrv.datasets.default_pathologies,out[0].detach().numpy())))
+output = {}
+with torch.no_grad():
+    img = torch.from_numpy(img).unsqueeze(0)
+    if cfg.cuda:
+        img = img.cuda()
+        model = model.cuda()
+        
+    if cfg.feats:
+        feats = model.features(img)
+        feats = F.relu(feats, inplace=True)
+        feats = F.adaptive_avg_pool2d(feats, (1, 1))
+        output["feats"] = list(feats.cpu().detach().numpy().reshape(-1))
+
+    preds = model(img).cpu()
+    output["preds"] = dict(zip(xrv.datasets.default_pathologies,preds[0].detach().numpy()))
+    
+if cfg.feats:
+    print(output)
+else:
+    pprint.pprint(output)
     
    
