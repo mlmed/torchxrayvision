43c58ee59422901b150f2dd5898a2a813c548b7c
Joseph Paul Cohen <joseph@josephpcohen.com>
2020-05-31 10:23:15 -0400

    update COVID dataset labels

--- torchxrayvision/datasets.py
+++ torchxrayvision/datasets.py
@@ -985,15 +985,15 @@ class COVID19_Dataset(Dataset):
         self.views = views
         
         # defined here to make the code easier to read
-        pneumonias = ["COVID-19", "SARS", "MERS", "ARDS", "Streptococcus", "Pneumocystis", "Klebsiella", "Chlamydophila", "Legionella"]
+        pneumonias = ["COVID-19", "SARS", "MERS", "ARDS", "Streptococcus", "Pneumocystis", "Klebsiella", "Chlamydophila", "Legionella", "Lipoid", "Influenza", "Mycoplasma", "Varicella"]
         
         self.pathologies = ["Pneumonia","Viral Pneumonia", "Bacterial Pneumonia", "Fungal Pneumonia", "No Finding"] + pneumonias
         self.pathologies = sorted(self.pathologies)
 
         mapping = dict()
         mapping["Pneumonia"] = pneumonias
-        mapping["Viral Pneumonia"] = ["COVID-19", "SARS", "MERS"]
-        mapping["Bacterial Pneumonia"] = ["Streptococcus", "Klebsiella", "Chlamydophila", "Legionella"]
+        mapping["Viral Pneumonia"] = ["COVID-19", "SARS", "MERS", "Influenza", "Varicella"]
+        mapping["Bacterial Pneumonia"] = ["Streptococcus", "Klebsiella", "Chlamydophila", "Legionella", "Mycoplasma"]
         mapping["Fungal Pneumonia"] = ["Pneumocystis"]
         
         # Load data
