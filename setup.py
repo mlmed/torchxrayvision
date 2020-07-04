f7636678d38ed0686118252a9c12961865e2fd57
Joseph Paul Cohen <joseph@josephpcohen.com>
2020-07-03 21:47:17 -0400

    increase version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="torchxrayvision",
-    version="0.0.9",
+    version="0.0.10",
     author="Joseph Paul Cohen",
     author_email="joseph@josephpcohen.com",
     description="A small example package",
