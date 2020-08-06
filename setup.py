d0260ec91068487e2ae9533eb31a0552c40b5354
Joseph Paul Cohen <joseph@josephpcohen.com>
2020-08-06 12:42:39 -0400

    patch loading old models

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="torchxrayvision",
-    version="0.0.11",
+    version="0.0.12",
     author="Joseph Paul Cohen",
     author_email="joseph@josephpcohen.com",
     description="TorchXrayVision: A library of chest X-ray datasets and models",
