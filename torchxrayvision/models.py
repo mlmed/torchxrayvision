fix bug with gradients when nans exist for pretrained models

torchxrayvision/models.py
@@ -224,14 +224,22 @@ def op_norm(outputs, op_threshs):
     Returns:
         outputs_new: normalized outputs, torch.Size(batch_size, num_tasks)
     """
+    #expand to batch size so we can do parallel comp
     op_threshs = op_threshs.expand(outputs.shape[0],-1)
+    
+    #initial values will be 0
     outputs_new = torch.zeros(outputs.shape, device = outputs.device)
-    mask_leq = outputs<op_threshs
+    
+    # only select non-nan elements otherwise the gradient breaks
+    mask_leq = (outputs<op_threshs)# & ~torch.isnan(op_threshs)
+    mask_gt = ~(outputs<op_threshs)# & ~torch.isnan(op_threshs)
+    
+    #scale outputs less than thresh
     outputs_new[mask_leq] = outputs[mask_leq]/(op_threshs[mask_leq]*2)
-    outputs_new[~mask_leq] = 1.0 - ((1.0 - outputs[~mask_leq])/((1-op_threshs[~mask_leq])*2))
+    #scale outputs greater than thresh
+    outputs_new[mask_gt] = 1.0 - ((1.0 - outputs[mask_gt])/((1-op_threshs[mask_gt])*2))
     
     return outputs_new
-
     
 def get_densenet_params(arch):
     assert 'dense' in arch