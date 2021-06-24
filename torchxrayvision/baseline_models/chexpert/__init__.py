bd9264ec7f915aaa8cd3c4523ea6f77ace90d8de
Joseph Paul Cohen <joseph@josephpcohen.com>
2021-06-24 09:41:39 -0700

    handle batches in the worst way ever, sorry

--- torchxrayvision/baseline_models/chexpert/__init__.py
+++ torchxrayvision/baseline_models/chexpert/__init__.py
@@ -22,25 +22,28 @@ class DenseNet(nn.Module):
     AAAI Conference on Artificial Intelligence. 
     http://arxiv.org/abs/1901.07031
     
+    Setting num_models less than 30 will load a subset of the ensemble.
+    
     Modified for torchxrayvision to maintain the pytorch gradient tape 
     and also to provide the features() argument.
     """
 
-    def __init__(self, weights_zip=""):
+    def __init__(self, weights_zip="", num_models=30):
         
         super(DenseNet, self).__init__()
 
         url = "https://archive.org/download/torchxrayvision_chexpert_weights/chexpert_weights.zip"
+        self.weights_zip = weights_zip
+        self.num_models = num_models
         
-        
-        if weights_zip == "":
+        if self.weights_zip == "":
             raise Exception("Need to specify weights_zip file location. You can download them from {}".format(url))
         
         self.use_gpu = torch.cuda.is_available()
         dirname = os.path.dirname(os.path.realpath(__file__))
         self.model = Tasks2Models(os.path.join(dirname, 'predict_configs.json'), 
-                             weights_zip=weights_zip,
-                             num_models=30, 
+                             weights_zip=self.weights_zip,
+                             num_models=self.num_models, 
                              dynamic=False, 
                              use_gpu=self.use_gpu)
 
@@ -58,18 +61,19 @@ class DenseNet(nn.Module):
         x = x/512
         #now between [-2,2] for this model
         
+        outputs = []
+        for sample in x: #sorry hard to make parallel
+            all_task2prob = {}
+            for tasks in self.model:
+                task2prob = self.model.infer(sample.unsqueeze(0), tasks)
+                for task, task_prob in task2prob.items():
+                    all_task2prob[task] = task_prob
+
+            output = [all_task2prob[patho] for patho in ["Atelectasis", "Cardiomegaly", "Consolidation", "Edema", "Pleural Effusion"]]
+            output = torch.stack(output)
+            outputs.append(output)
         
-        all_task2prob = {}
-        for tasks in self.model:
-            task2prob = self.model.infer(x, tasks)
-            #return task2prob
-            for task, task_prob in task2prob.items():
-                all_task2prob[task] = task_prob
-                
-        output = [all_task2prob[patho] for patho in ["Atelectasis", "Cardiomegaly", "Consolidation", "Edema", "Pleural Effusion"]]
-        output = torch.stack(output)
-            
-        return output
+        return torch.stack(outputs)
     
     def features(self, x):
         x = x.repeat(1, 3, 1, 1)
@@ -79,13 +83,16 @@ class DenseNet(nn.Module):
         x = x/512
         #now between [-2,2] for this model
         
-        all_feats = []
-        for tasks in self.model:
-            task2prob = self.model.features(x, tasks)
-            all_feats.append(task2prob)
-           
-        #return all_feats
-        return torch.stack(all_feats)
+        outputs = []
+        for sample in x: #sorry hard to make parallel
+            all_feats = []
+            for tasks in self.model:
+                task2prob = self.model.features(x, tasks)
+                print(task2prob.shape)
+                all_feats.append(task2prob)
+            outputs.append(torch.stack(all_feats))
+
+        return torch.stack(outputs)
     
     
     def __repr__(self):
