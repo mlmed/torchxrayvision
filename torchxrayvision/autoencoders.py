Fix autoencoder variable assignment to address gradient error (#83)
* fix autoencoder variable assignment to address gradient error

* test on pytorch 1.10

* test on pytorch 1.10.0

torchxrayvision/autoencoders.py
@@ -52,7 +52,7 @@ class Bottleneck(nn.Module):
         if self.downsample is not None:
             shortcut = self.downsample(x)
 
-        out += shortcut
+        out = out + shortcut
         out = self.relu(out)
 
         return out
@@ -99,7 +99,7 @@ class DeconvBottleneck(nn.Module):
         if self.upsample is not None:
             shortcut = self.upsample(x)
 
-        out += shortcut
+        out = out + shortcut
         out = self.relu(out)
 
         return out