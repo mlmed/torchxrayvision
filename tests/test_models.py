1448fc1d74ff8bb634e565895b288cc357916b2c
Joseph Paul Cohen <joseph@josephpcohen.com>
2022-03-03 11:13:43 -0800

    improve test

--- tests/test_models.py
+++ tests/test_models.py
@@ -95,6 +95,11 @@ def test_normalization_check():
         for ra in incorrect_ranges:
             test_x = torch.zeros([1,1,224,224])
             test_x.uniform_(ra[0], ra[1])
+            
+            # Sometimes uniform_ doesn't hit the edge cases we want
+            # so here the first 2 pixels are set to the limits
+            test_x[0][0][0] = ra[0]  
+            test_x[0][0][1] = ra[1]
             xrv.models.warning_log = {}
             model(test_x)
             assert xrv.models.warning_log['norm_correct'] == False, ra
