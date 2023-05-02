d4aa6f1b7e82e8c537820f905f820c2ae858ccab
Joseph Paul Cohen <joseph@josephpcohen.com>
2023-05-01 18:48:29 -0700

    add tests

--- tests/test_baseline_models.py
+++ tests/test_baseline_models.py
@@ -14,10 +14,13 @@ def test_baselinemodel_jfhealthcare_function():
     
     model = xrv.baseline_models.jfhealthcare.DenseNet()
     
-    img = torch.ones(1,1,224,224)
+    img = torch.ones(1, 1, 224, 224)
     img.requires_grad = True
     pred = model(img)[:,model.pathologies.index("Cardiomegaly")]
+    assert pred.shape == torch.Size([1]), 'check output is correct shape'
+        
     dzdxp = torch.autograd.grad((pred), img)[0]
+    assert dzdxp.shape == torch.Size([1, 1, 224, 224]), 'check grads are the correct size'
     
     assert torch.isnan(dzdxp.flatten()).sum().cpu().numpy() == 0 
     
@@ -26,10 +29,28 @@ def test_baselinemodel_emory_hiti_function():
     
     model = xrv.baseline_models.emory_hiti.RaceModel()
     
-    img = torch.ones(1,1,224,224)
+    img = torch.ones(1, 1, 224, 224)
     img.requires_grad = True
     pred = model(img)[:,model.targets.index("White")]
+    assert pred.shape == torch.Size([1]), 'check output is correct shape'
     dzdxp = torch.autograd.grad((pred), img)[0]
+    assert dzdxp.shape == torch.Size([1, 1, 224, 224]), 'check grads are the correct size'
     
     assert torch.isnan(dzdxp.flatten()).sum().cpu().numpy() == 0 
     
+    
+def test_baselinemodel_riken_age_function():
+    
+    model = xrv.baseline_models.riken.AgeModel()
+
+    img = torch.ones(2, 1, 224, 224)
+    img.requires_grad = True
+    pred = model(img)
+    assert pred.shape == torch.Size([2, 1]), 'check output is correct shape'
+    assert pred[0] > 0 and pred[0] < 100, 'check output is in the correct range'
+
+    dzdxp = torch.autograd.grad(pred.sum(), img)[0]
+
+    assert dzdxp.shape == torch.Size([2, 1, 224, 224]), 'check grads are the correct size'
+
+    assert torch.isnan(dzdxp.flatten()).sum().cpu().numpy() == 0 , 'check no grads are nans'
